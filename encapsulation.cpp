6e96931eecf6a1cc89615bf15efeb21f6bc8b2d6
Mahi Sarwar Anol <91027428+Mahi-Anol@users.noreply.github.com>
2024-03-03 20:48:46 +0600

    Create encapsulation.cpp

---
+++ encapsulation.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+class instrument{
+
+    private:
+    string name;
+    string colour;
+    int Size;
+    static int n_obj;
+
+    public:
+
+    instrument(){
+        (*this).name="Not declared";
+        this->colour="Not declared";
+        this->Size=-1;
+        n_obj++;
+        };
+
+    instrument(string name,string colour,int Size){
+        this->name=name;
+        this->colour=colour;
+        this->Size=Size;
+        n_obj++;
+    };
+
+    void display(void)
+    {
+        cout<<"Instrument Number = "<<n_obj<<endl;
+        cout<<"Name = "<<name<<endl;
+        cout<<"colour= "<<colour<<endl;
+        cout<<"Size = "<<Size<<endl;
+        cout<<endl<<endl<<endl;
+    }
+};
+
+int instrument::n_obj=0;
+
+
+int main(void)
+{
+  cout<<"He welcome to oop"<<endl;
+  cout<<"Lets try encapsulation"<<endl;
+  cout<<setw(20)<<"******"<<endl;
+  cout<<endl<<endl;
+
+  instrument b("Guitar","Red",3);
+  b.display();
+
+  instrument c,d,e;
+
+  e.display();
+
+  return 0;
+}
